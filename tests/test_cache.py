5bac6a3b27ec5eacce1c39f18f1b98b3464be255
o-hayato <preasper0+github@gmail.com>
2018-08-19 11:02:11 +0900

    add in memory cache
    タイムアウトでの再実行高速化のため

---
+++ tests/test_cache.py
@@ -0,0 +1,30 @@
+import pytest
+from memorize.cache import Cache
+from memorize.cache import cached
+from time import sleep
+
+
+@cached()
+def example_func(url):
+    return url + "_example"
+
+
+def test_timeout():
+    cache = Cache(timeout=0.1)
+    cache.set("key", "val")
+    sleep(0.1)
+    val = cache.get("key")
+    assert val is None
+
+
+def test_get():
+    cache = Cache()
+    cache.set("key", "val")
+    val = cache.get("key")
+    assert val == "val"
+
+
+def test_cached_func():
+    example_func(url="aaaa")
+    example_func(url="aaaa")
+    example_func("aaaa")
